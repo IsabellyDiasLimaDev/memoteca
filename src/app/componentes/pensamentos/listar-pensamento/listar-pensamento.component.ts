b94cd72f23bf2b5b4454d571d0a616a32776e860
Isabelly Dias <isabellydias29@gmail.com>
2023-04-18 19:00:17 -0300

    Aula 3 - Implementando a paginação

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -8,16 +8,26 @@ import { Pensamento } from '../pensamento/pensamento';
   styleUrls: ['./listar-pensamento.component.css'],
 })
 export class ListarPensamentoComponent implements OnInit {
+
   listaPensamentos: Pensamento[] = [];
+  paginaAtual: number = 1;
+  haMaisPensamentos: boolean = true;
 
   constructor(private service: PensamentoService) {}
 
   // faz parte do ciclo de vida do componente
   ngOnInit(): void {
-    this.service.listar().subscribe((listaPensamentos) => {
+    this.service.listar(this.paginaAtual).subscribe((listaPensamentos) => {
       this.listaPensamentos = listaPensamentos;
     });
+  }
 
-    
+  carregarMaisPensamentos() {
+    this.service.listar(++this.paginaAtual).subscribe(listaPensamentos => {
+      this.listaPensamentos.push(...listaPensamentos);
+      if (!listaPensamentos.length) {
+        this.haMaisPensamentos = false;
+      }
+    });
   }
 }
